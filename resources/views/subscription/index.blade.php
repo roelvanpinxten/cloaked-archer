25325507bacf82be95233623cbe01bdb2dd29f3e
Roel van Pinxten <roelvanpinxten@gmail.com>
2015-04-14 22:39:43 +0200

    new files

---
+++ resources/views/subscription/index.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section ('menu')
+    @include('partials.appMenu')
+@stop()
+
+@section('content')
+
+    <div class="container">
+        <p>Alle Subsctiptions.</p>
+     </div>
+
+@stop()
